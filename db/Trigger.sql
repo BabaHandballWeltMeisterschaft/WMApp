5897b6b6933faa830f6fbb495c8a650950a86dc5
Michellllllllll <123157863+Michellllllllll@users.noreply.github.com>
2023-02-14 14:16:35 +0100

    Add files via upload

--- db/Trigger.sql
+++ db/Trigger.sql
@@ -12,11 +12,11 @@ drop trigger Punkte;
 CREATE TRIGGER Punkte AFTER Insert ON SPIEL
 for each row
 begin
-			insert into test values (new.TEAM1_ID,
+			insert into test(team_id,punkte) values (new.TEAM1_ID,
 				case when new.TOR_TEAM1 > new.TOR_TEAM2 Then '2'
 			         when new.TOR_TEAM1 < new.TOR_TEAM2 Then '0'	
 					 when new.TOR_TEAM1 = new.TOR_TEAM2 Then '1' END);
-			insert into test values (new.TEAM2_ID,
+			insert into test(team_id,punkte) values (new.TEAM2_ID,
 				case when new.TOR_TEAM1 > new.TOR_TEAM2 Then '0'
 			         when new.TOR_TEAM1 < new.TOR_TEAM2 Then '2'	
 					 when new.TOR_TEAM1 = new.TOR_TEAM2 Then '1' END);
