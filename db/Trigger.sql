5371c77ed9012ae1617ca96356673572bb53d41b
Michellllllllll <123157863+Michellllllllll@users.noreply.github.com>
2023-02-14 14:10:13 +0100

    Add files via upload

---
+++ db/Trigger.sql
@@ -0,0 +1,26 @@
+drop table test;
+create table test
+(
+ id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+ team_id int,
+ punkte int
+);
+
+
+drop trigger Punkte;
+
+CREATE TRIGGER Punkte AFTER Insert ON SPIEL
+for each row
+begin
+			insert into test values (new.TEAM1_ID,
+				case when new.TOR_TEAM1 > new.TOR_TEAM2 Then '2'
+			         when new.TOR_TEAM1 < new.TOR_TEAM2 Then '0'	
+					 when new.TOR_TEAM1 = new.TOR_TEAM2 Then '1' END);
+			insert into test values (new.TEAM2_ID,
+				case when new.TOR_TEAM1 > new.TOR_TEAM2 Then '0'
+			         when new.TOR_TEAM1 < new.TOR_TEAM2 Then '2'	
+					 when new.TOR_TEAM1 = new.TOR_TEAM2 Then '1' END);
+
+END;
+
+
