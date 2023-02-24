0d46e846130d8a8d0dba8f0fc5736847c06d2068
Michellllllllll <123157863+Michellllllllll@users.noreply.github.com>
2023-02-24 21:08:22 +0100

    Add files via upload

--- db/Trigger.sql
+++ db/Trigger.sql
@@ -1,5 +1,5 @@
-drop table test;
-create table test
+drop table TMP;
+create table TMP
 (
  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
  team_id int,
@@ -12,14 +12,20 @@ drop trigger Punkte;
 CREATE TRIGGER Punkte AFTER Insert ON SPIEL
 for each row
 begin
-			insert into test(team_id,punkte) values (new.TEAM1_ID,
+			insert into TMP(team_id,punkte) values (new.TEAM1_ID,
 				case when new.TOR_TEAM1 > new.TOR_TEAM2 Then '2'
 			         when new.TOR_TEAM1 < new.TOR_TEAM2 Then '0'	
 					 when new.TOR_TEAM1 = new.TOR_TEAM2 Then '1' END);
-			insert into test(team_id,punkte) values (new.TEAM2_ID,
+			insert into TMP(team_id,punkte) values (new.TEAM2_ID,
 				case when new.TOR_TEAM1 > new.TOR_TEAM2 Then '0'
 			         when new.TOR_TEAM1 < new.TOR_TEAM2 Then '2'	
 					 when new.TOR_TEAM1 = new.TOR_TEAM2 Then '1' END);
+					 
+			UPDATE TEAM
+				SET PUNKTE = PUNKTE +(SELECT SUM(punkte) FROM TMP WHERE team_id = TEAM.TEAM_ID)
+				WHERE EXISTS (SELECT 1 FROM TMP WHERE team_id = TEAM.TEAM_ID);
+				
+			DELETE FROM TMP;
 
 END;
 
