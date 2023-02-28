dab69160920cce329b03d1d56509cec05f1575a4
Michellllllllll <123157863+Michellllllllll@users.noreply.github.com>
2023-02-28 08:11:34 +0100

    Add files via upload

--- db/Team.sql
+++ db/Team.sql
@@ -9,42 +9,42 @@ CREATE TABLE TEAM (
 
 INSERT INTO TEAM(GRUPPE, NAME, PUNKTE)
 VALUES
-(1,'Spanien',6),
-(1,'Montenegro',4),
-(1,'Iran',2),
+(1,'Spanien',0),
+(1,'Montenegro',0),
+(1,'Iran',0),
 (1,'Chile',0),
 
-(2,'Frankreich',6),
-(2,'Slowenien',4),
-(2,'Polen',2),
+(2,'Frankreich',0),
+(2,'Slowenien',0),
+(2,'Polen',0),
 (2,'Saudi-Arabien',0),
 
-(3,'Schweden',6),
-(3,'Brasilien',4),
-(3,'Kap Verde',2),
+(3,'Schweden',0),
+(3,'Brasilien',0),
+(3,'Kap Verde',0),
 (3,'Uruguay',0),
 
-(4,'Portugal',4),
-(4,'Island',4),
-(4,'Ungarn',4),
+(4,'Portugal',0),
+(4,'Island',0),
+(4,'Ungarn',0),
 (4,'Südkorea',0),
 
-(5,'Deutschland',6),
-(5,'Serbien',4),
-(5,'Katar',2),
+(5,'Deutschland',0),
+(5,'Serbien',0),
+(5,'Katar',0),
 (5,'Algerien',0),
 
-(6,'Norwegen',6),
-(6,'Niederlande',4),
-(6,'Argentienen',2),
+(6,'Norwegen',0),
+(6,'Niederlande',0),
+(6,'Argentienen',0),
 (6,'Nordmazedonien',0),
 
-(7,'Ägypten',6),
-(7,'Kroatien',4),
-(7,'USA',2),
+(7,'Ägypten',0),
+(7,'Kroatien',0),
+(7,'USA',0),
 (7,'Marokko',0),
 
-(8,'Dänemark',6),
-(8,'Bahrain',3),
-(8,'Belgien',2),
-(8,'Tunesien',1);
+(8,'Dänemark',0),
+(8,'Bahrain',0),
+(8,'Belgien',0),
+(8,'Tunesien',0);
